8265dfa51b496c592f90b1349c13f583e5828b99
unknown <ras81@.(none)>
2009-10-21 12:39:18 +0100

    made files import database info no that it can be kept private

---
+++ privatetemplate.py
@@ -0,0 +1,19 @@
+# This file contains all the information that should not be put in the
+# public domain.
+# make a copy of this file called private.py and put your login data in 
+# Do not version control it.
+# There maybe some scripts that have the old details in and dont import private.
+
+
+defaulthost = "writehosthere"
+defaultuser = "writeuserher"
+defaultpasswd = "writepasswordhere"
+defaultdb = "writedbhere"
+
+
+
+
+
+
+
+
