4290fb58120b6175db0d577a18ce3ae749e56c45
Raphael Shirley <ras81>
2009-11-25 16:16:19 +0000

    changed general.py so it initialized constant and unitconverter objects
    Still slightly confused by why you can call some things directly from the class. I think this may only be possible in python's OOP methodology

--- general.py
+++ general.py
@@ -5,6 +5,8 @@
 
 
 ############### PACKAGES ###################
+#IMPORT ALL THE PACKAGES USED IN ANY OF THE THERMO SCRIPTS
+#(THESE MUST ALL BE INSTALLED)
 import MySQLdb
 import openbabel, pybel
 from Cantera import *
@@ -23,20 +25,25 @@ class constants:
     kb = 1.3806503E-23         #Boltzman's constant SI
     h  = 6.626068E-34          #Planck's constant   SI
     c  = 299792458             #Speed of light      SI
-    Na = 6.0221415E23          #Avogadro's Constant 
+    Na = 6.0221415E23          #Avogadro's Number 
     amu = 1.66053886E-27       #mass of carbon12/12 SI  used to be called c12over12
     a0 = 5.29177E-11           #bohr radius         SI
     atm = 101325               #1atm = 101325Pa     SI
     hbar = 6.626068E-34 /(2*pi)#h over 2 pi         SI
+
+constants = constants()   # initialize object (instance of the class constants -
+                          # doesn't matter that the names are the same 
 ############### CONSTANTS ##################
 
 ################ UNIT CONVERTERS ############
+#e.g. to get 4.0 cm^-1 in to Hz write unitconverter.cmtohz(4.0)
 class unitconverter:
     """Class defining a number of functions to convert between units"""
-    def cmtohz:
-        
+    def cmtohz(self,cm):
+        result = cm*100*constants.h*constants.c/constants.kb
+        return result
+    
+unitconverter = unitconverter() # initialize object (instance of the class unitconverter -
+                                # doesn't matter that the names are the same 
 
-
-#convertcm = 100*h*c/kb # converts the wavenumbers for powers ie convert to joules and divide by k.
-#hbar = h/(2*pi)
-################ UNIT CONVERTERS ############
+############### UNIT CONVERTERS ############
